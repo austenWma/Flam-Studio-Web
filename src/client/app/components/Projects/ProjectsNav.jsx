451716bc6aa8000670717ae08f1ceff902cebda0
Austen Ma <austenma@Austens-MacBook-Pro.local>
2018-02-04 16:09:39 -0800

    Added share button and settings icon on project nav bar

--- src/client/app/components/Projects/ProjectsNav.jsx
+++ src/client/app/components/Projects/ProjectsNav.jsx
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 import {Redirect, Link} from 'react-router-dom'
 
+import SettingsApp from 'material-ui/svg-icons/action/settings';
+import UploadIcon from 'material-ui/svg-icons/action/backup';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconButton from 'material-ui/IconButton';
+
 class ProjectsNav extends Component {
   constructor (props) {
     super(props)
@@ -13,6 +18,20 @@ class ProjectsNav extends Component {
     return (
       <div className="projectsNavContainer">
         <div className="projectsNavContainerStudioTitle" onClick={this.props.goToStudioHome}>Flam Studio</div>
+        <div className="projectsNavUtilitiesContainer">
+          <MuiThemeProvider>
+          <div className="projectsNavUtilitiesIcons">
+            <IconButton iconStyle={{color: 'white', float: 'right'}}>
+              <SettingsApp />
+            </IconButton>
+            <div className="projectsNavUtilitiesShareButtonContainer" >
+              <div className="projectsNavUtilitiesShareButton">
+                <UploadIcon style={{color: 'white', marginRight: 5}}/> Share
+              </div>
+            </div>
+          </div>
+          </MuiThemeProvider>
+        </div>
       </div>
     )
   }
